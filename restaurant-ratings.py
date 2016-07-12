eee31b932b7b4589c46a7538a7b994d301918dee
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:54:24 -0700

    Almost finished program

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,17 @@
-# your code goes here
+score = {}  
+input_file = open("scores.txt")
+for line in input_file:
+    line = line.rstrip()
+    line = line.split(":")
+    score[line[0]]=line[1] 
+
+for item in score:
+    "%s is rated at %d" % item, score[item] 
+
+
+
+
+# create a list of keys
+# sort the keys
+# for each key in the sorted keys:
+#     print the restaurant name and score# your code goes here
